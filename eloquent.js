d640d49022b5761af76ba067535fc4255855660c
Mahuta <mahutahs@yahoo.co.nz>
2016-03-06 14:30:09 +1300

    added finished eloquent file

---
+++ eloquent.js
@@ -0,0 +1,37 @@
+//*Minimum*//
+
+function min(a, b) {
+	return Math.min(a , b);
+}
+
+//*Recursion*//
+function isEven(x) {
+	if (x === 0) {
+		return true;
+	} else if (x === 1) {
+		return false;
+	} else if (x > 0) {
+		return isEven(x - 2);
+	} else if (x < 0) {
+		return isEven(x + 2);
+	}
+
+
+}
+
+//*Bean counting*//
+function countBs(str) {
+	var strArray = str.split("");
+	var count = strArray.filter(function(x){
+		return x === "B";
+	}).length;
+	return count;
+}
+
+function countChar(str, character) {
+	var strArray = str.split("");
+	var count = strArray.filter(function(x){
+		return x === character;
+	}).length;
+	return count;
+}
